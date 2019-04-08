e8413570dcd9fe1fdda2f1e177d6cd9eb8cde0bd
ross-donohue <ross.donohue@gmail.com>
2019-04-07 22:01:10 -0400

    Update cachematrix.R
    Wrote the requested functions to demonstrate understanding of scoping rules and wrote comments to describe what the written functions do

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix saves the matrix provided in the environment for a list of functions
+## these function provide options to a) replace the matrix, b) retrive the matrix, 
+## c) establish an inverse matrix, or d) retrieve the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ I <- NULL
+ set <- function(y){
+         x <<- y
+         I <<- NULL
+ }
+ get <- function() x
+ setinverse <- function(inverse) I <<- inverse
+ getinverse <- function() I
+ list(set = set, get = get, 
+      setinverse = setinverse,
+      getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks to see if the Inverse has already been calculated 
+## since the last change and either: retrieves the cached inverse matrix, or
+## calculates the new inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        I <- x$getinverse()
+        if(!is.null(I)){
+                message("retrieving cached inverse matrix")
+                return(I)
+        }
+        matrix <- x$get()
+        I <- solve(matrix)
+        x$setinverse(I)
+        I
 }
